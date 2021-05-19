6e16d6b1ddc6e308d50ac8244e360c441c8068d5
dhhruv <180320107529.ce.dhruv@gmail.com>
2021-05-19 12:51:21 +0530

    captcha.py Updated

--- utils/captcha.py
+++ utils/captcha.py
@@ -5,7 +5,7 @@ from reportlab.graphics import renderPM
 from svglib.svglib import svg2rlg
 
 
-def captchaBuilder(resp):
+def captcha_builder(resp):
     with open("captcha.svg", "w") as f:
         f.write(re.sub('(<path d=)(.*?)(fill="none"/>)', "", resp["captcha"]))
 
@@ -15,13 +15,13 @@ def captchaBuilder(resp):
     layout = [
         [sg.Image("captcha.png")],
         [sg.Text("Enter Captcha Below")],
-        [sg.Input(key="txtcaptcha")],
+        [sg.Input(key="inp")],
         [sg.Button("Submit", bind_return_key=True)],
     ]
 
     window = sg.Window("Enter Captcha", layout, finalize=True)
     window.TKroot.focus_force()  # focus on window
-    window.Element("txtcaptcha").SetFocus()  # focus on field
+    window.Element("inp").SetFocus()  # focus on field
     event, values = window.read()
     window.close()
-    return values["txtcaptcha"]
+    return values["inp"]
